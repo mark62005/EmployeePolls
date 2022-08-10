5bcbdff17858cec9de8d8a6d10ca4b57a44d5cc6
Mark Wong <markwong.digital@gmail.com>
2022-08-10 15:49:23 -0700

    Fix format

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import '../css/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Employee Polls
-    </div>
-  );
+    return (
+        <div className="App">
+            Employee Polls
+        </div>
+    );
 }
 
 export default App;
