eee3c9af7656dd2083dadeacc992eb0b60063619
Mark Wong <markwong.digital@gmail.com>
2022-08-10 16:51:37 -0700

    Create store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './components/App';
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({ reducer, middleware });
+
 root.render(
-    <React.StrictMode>
+    <Provider store={ store }>
         <App />
-    </React.StrictMode>
+    </Provider>
 );
