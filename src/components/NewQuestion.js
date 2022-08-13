b56ea6b7d54dd8d076025fe3da1bebe728ed2b1e
Mark Wong <markwong.digital@gmail.com>
2022-08-12 23:25:38 -0700

    Create UI of NewQuestion with Bootstrap

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -4,6 +4,11 @@ import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { OPTION_ONE, OPTION_TWO } from "../utils/helpers";
 import { handleAddQuestion } from "../actions/shared";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const NewQuestion = ({ dispatch }) => {
     const navigate = useNavigate();
@@ -33,18 +38,45 @@ const NewQuestion = ({ dispatch }) => {
     };
 
     return (
-        <div>
-            <h2>Would You Rather</h2>
-            <h3>Create Your Own Poll</h3>
-            <form onSubmit={ handleSubmit }>
-                <label id={ OPTION_ONE }>First Option</label>
-                <input name={ OPTION_ONE } value={ optionOneText } placeholder="Option One" onChange={ handleInputChange } />
-                <label id={ OPTION_TWO }>Second Option</label>
-                <input name={ OPTION_TWO } value={ optionTwoText } placeholder="Option Two" onChange={ handleInputChange } />
+        <Card className="text-center m-4">
+            <Card.Header>Would You Rather</Card.Header>
+            <Card.Body>
+                <Card.Title>Create Your Own Poll</Card.Title>
+                <Form onSubmit={ handleSubmit } className="m-3">
+                    <Row className="mb-3">
+                        <Form.Group as={ Col } controlId={ OPTION_ONE }>
+                            <Form.Label>Option One</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name={ OPTION_ONE }
+                                placeholder="Enter Option One"
+                                value={ optionOneText }
+                                onChange={ handleInputChange }
+                            />
+                        </Form.Group>
 
-                <button type="submit" disabled={ optionOneText === "" || optionTwoText === "" }>Create</button>
-            </form>
-        </div>
+                        <Form.Group as={ Col } controlId={ OPTION_TWO }>
+                            <Form.Label>Option Two</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name={ OPTION_TWO }
+                                placeholder="Enter Option Two"
+                                value={ optionTwoText }
+                                onChange={ handleInputChange }
+                            />
+                        </Form.Group>
+                    </Row>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        disabled={ optionOneText === "" || optionTwoText === "" }
+                    >
+                        Submit
+                    </Button>
+                </Form>
+
+            </Card.Body>
+        </Card>
     );
 };
 
