3c3fd725758dc5f07d92d3bfa8f7aeca9e14ed1d
Mark Wong <markwong.digital@gmail.com>
2022-08-13 19:51:03 -0700

    Add test for setAuthedUser

---
+++ src/actions/test/authedUser.test.js
@@ -0,0 +1,12 @@
+import { SET_AUTHED_USER, setAuthedUser } from "../authedUser";
+
+describe("setAuthedUser", () => {
+    it(`will return an object with type of ${SET_AUTHED_USER} and user ID`, () => {
+        const id = "sarahedo";
+
+        expect(setAuthedUser(id)).toEqual({
+            type: SET_AUTHED_USER,
+            id,
+        });
+    });
+});
